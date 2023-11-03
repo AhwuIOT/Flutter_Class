c684dea48a536c61fc3f50340d925a6cdd95f302
AhwuIOT <ahwuiot@gmail.com>
2023-11-03 17:14:39 +0800

    multiple

--- Practice/counter/lib/main.dart
+++ Practice/counter/lib/main.dart
@@ -16,262 +16,280 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class TimerItem {
+  int hours;
+  int minutes;
+  int seconds;
+  bool isActive;
+  TextEditingController hourController;
+  TextEditingController minuteController;
+  TextEditingController secondController;
+  Timer? timer;
+
+  TimerItem({
+    this.hours = 0,
+    this.minutes = 0,
+    this.seconds = 0,
+    this.isActive = false,
+    TextEditingController? hourController,
+    TextEditingController? minuteController,
+    TextEditingController? secondController,
+  })  : hourController = hourController ?? TextEditingController(),
+        minuteController = minuteController ?? TextEditingController(),
+        secondController = secondController ?? TextEditingController();
+
+  void startTimer() {
+    const oneSec = Duration(seconds: 1);
+    timer = Timer.periodic(oneSec, (Timer timer) {
+      // Timer tick update logic will be added here
+    });
+  }
+
+  void stopTimer() {
+    if (timer != null) {
+      timer!.cancel();
+      isActive = false;
+    }
+  }
+
+  void resetTimer() {
+    stopTimer();
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+    hourController.clear();
+    minuteController.clear();
+    secondController.clear();
+  }
+
+  // Other methods like continueTimer will be added here if needed
+}
+
 class TimerApp extends StatefulWidget {
   @override
   _TimerAppState createState() => _TimerAppState();
 }
 
 class _TimerAppState extends State<TimerApp> {
-  int _hours1 = 0;
-  int _minutes1 = 0;
-  int _seconds1 = 0;
-  bool _isActive1 = false;
-  int _hours2 = 0;
-  int _minutes2 = 0;
-  int _seconds2 = 0;
-  bool _isActive2 = false;
-  TextEditingController _hourController1 = TextEditingController();
-  TextEditingController _minuteController1 = TextEditingController();
-  TextEditingController _secondController1 = TextEditingController();
-  TextEditingController _hourController2 = TextEditingController();
-  TextEditingController _minuteController2 = TextEditingController();
-  TextEditingController _secondController2 = TextEditingController();
-  late Timer _timer1;
-  late Timer _timer2;
-
+  List<TimerItem> timers = [];
+  double _timerfontsize = 30.0;
+  bool changeFontSize = false;
   @override
   void dispose() {
-    _timer1.cancel();
-    _timer2.cancel();
+    for (var timerItem in timers) {
+      timerItem.stopTimer();
+    }
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter多個計數器'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // 計數器1
-            Text('計數器1: $_hours1 小時 $_minutes1 分 $_seconds1 秒'),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  width: 80,
-                  child: TextFormField(
-                    controller: _hourController1,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(labelText: '小時'),
-                  ),
-                ),
-                SizedBox(width: 20),
-                SizedBox(
-                  width: 80,
-                  child: TextFormField(
-                    controller: _minuteController1,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(labelText: '分鐘'),
-                  ),
-                ),
-                SizedBox(width: 20),
-                SizedBox(
-                  width: 80,
-                  child: TextFormField(
-                    controller: _secondController1,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(labelText: '秒數'),
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      _resetTimer1();
-                    });
-                  },
-                  child: Text('重置計數器1'),
-                )
-              ],
-            ),
+        // appBar: AppBar(
+        //   title: Text('Dynamic Timers'),
+        // ),
+        body: ListView.builder(
+          itemCount: timers.length,
+          itemBuilder: (context, index) {
+            return Card(
+              child: Padding(
+                padding: const EdgeInsets.all(3.0),
+                child: Column(
+                  children: [
+                    LayoutBuilder(
+                      builder: (context, constraints) {
+                        // 在此处计算字体大小，例如基于容器的宽度和高度
+                        double fontSize =
+                            constraints.maxWidth / 5.0; // 调整这个比例以满足您的需求
 
-            // 計數器2
-            Text('計數器2: $_hours2 小時 $_minutes2 分 $_seconds2 秒'),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  width: 80,
-                  child: TextFormField(
-                    controller: _hourController2,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(labelText: '小時'),
-                  ),
+                        return Text(
+                          '${timers[index].hours}:${timers[index].minutes}:${timers[index].seconds}',
+                          style: TextStyle(
+                              color: Colors.amber,
+                              fontSize: fontSize,
+                              fontWeight: FontWeight.bold), // 使用计算的字体大小
+                        );
+                      },
+                    ),
+                    Visibility(
+                        visible: !timers[index].isActive,
+                        maintainSize: false,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              width: 80,
+                              child: TextFormField(
+                                controller: timers[index].hourController,
+                                keyboardType: TextInputType.number,
+                                decoration: InputDecoration(labelText: '小時'),
+                              ),
+                            ),
+                            SizedBox(width: 20),
+                            SizedBox(
+                              width: 80,
+                              child: TextFormField(
+                                controller: timers[index].minuteController,
+                                keyboardType: TextInputType.number,
+                                decoration: InputDecoration(labelText: '分鐘'),
+                              ),
+                            ),
+                            SizedBox(width: 20),
+                            SizedBox(
+                              width: 80,
+                              child: TextFormField(
+                                controller: timers[index].secondController,
+                                keyboardType: TextInputType.number,
+                                decoration: InputDecoration(labelText: '秒數'),
+                              ),
+                            ),
+                            Column(
+                              children: [
+                                TextButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        timers[index].hours = int.tryParse(
+                                                timers[index]
+                                                    .hourController
+                                                    .text) ??
+                                            0;
+                                        timers[index].minutes = int.tryParse(
+                                                timers[index]
+                                                    .minuteController
+                                                    .text) ??
+                                            0;
+                                        timers[index].seconds = int.tryParse(
+                                                timers[index]
+                                                    .secondController
+                                                    .text) ??
+                                            0;
+                                        timers[index].isActive = true;
+                                        _changeFontSize(timers[index]);
+                                      });
+                                    },
+                                    child: Text("SetTimer")),
+                                TextButton(
+                                  onPressed: () => _resetTimer(timers[index]),
+                                  child: Text('Reset'),
+                                ),
+                              ],
+                            ),
+                            Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  TextButton(
+                                    onPressed: () => _startTimer(timers[index]),
+                                    child: Text('Start'),
+                                  ),
+                                  ElevatedButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          timers.removeAt(index);
+                                        });
+                                      },
+                                      child: Icon(Icons.delete)),
+                                ]),
+                          ],
+                        )),
+                    SizedBox(
+                      height: 3,
+                    ),
+                  ],
                 ),
-                SizedBox(width: 20),
-                SizedBox(
-                  width: 80,
-                  child: TextFormField(
-                    controller: _minuteController2,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(labelText: '分鐘'),
-                  ),
-                ),
-                SizedBox(width: 20),
-                SizedBox(
-                  width: 80,
-                  child: TextFormField(
-                    controller: _secondController2,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(labelText: '秒數'),
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      _resetTimer2();
-                    });
-                  },
-                  child: Text('重置計數器2'),
-                )
-              ],
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  int inputHours1 = int.tryParse(_hourController1.text) ?? 0;
-                  int inputMinutes1 =
-                      int.tryParse(_minuteController1.text) ?? 0;
-                  int inputSeconds1 =
-                      int.tryParse(_secondController1.text) ?? 0;
-                  int inputHours2 = int.tryParse(_hourController2.text) ?? 0;
-                  int inputMinutes2 =
-                      int.tryParse(_minuteController2.text) ?? 0;
-                  int inputSeconds2 =
-                      int.tryParse(_secondController2.text) ?? 0;
-                  _hours1 = inputHours1;
-                  _minutes1 = inputMinutes1;
-                  _seconds1 = inputSeconds1;
-                  _hours2 = inputHours2;
-                  _minutes2 = inputMinutes2;
-                  _seconds2 = inputSeconds2;
-                  _startTimer();
-                });
-              },
-              child: Text('啟動計數器1和計數器2'),
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  _stopTimer();
-                });
-              },
-              child: Text('停止計數器1和計數器2'),
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  _continueTimer();
-                });
-              },
-              child: Text('繼續計數器1和計數器2'),
-            ),
-            SizedBox(height: 10),
-          ],
+              ),
+            );
+          },
         ),
-      ),
-    );
+        floatingActionButton: FloatingActionButton(
+          onPressed: addTimer,
+          tooltip: 'Add Timer',
+          child: Icon(Icons.add),
+        ),
+        bottomNavigationBar: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  for (var i = 0; i < timers.length; i++) {
+                    _startTimer(timers[i]);
+                  }
+                });
+              },
+              child: Text('Start All'),
+            ),
+
+            // SizedBox(
+            //   width: 50,
+            // ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  for (var i = 0; i < timers.length; i++) {
+                    _stopTimer(timers[i]);
+                  }
+                });
+              },
+              child: Text('Stop'),
+            ),
+          ],
+        ));
   }
 
-  void _startTimer() {
-    const oneSecond = Duration(seconds: 1);
-    _timer1 = Timer.periodic(oneSecond, (timer) {
-      setState(() {
-        if (_hours1 == 0 && _minutes1 == 0 && _seconds1 == 0) {
-          timer.cancel();
-          _isActive1 = false;
-        } else {
-          if (_seconds1 == 0) {
-            if (_minutes1 == 0) {
-              if (_hours1 > 0) {
-                _hours1--;
-                _minutes1 = 59;
-                _seconds1 = 59;
+  void addTimer() {
+    setState(() {
+      timers.add(TimerItem());
+    });
+  }
+
+  void _startTimer(TimerItem timerItem) {
+    const oneSec = Duration(seconds: 1);
+    timerItem.timer = Timer.periodic(oneSec, (Timer timer) {
+      if (timerItem.hours == 0 &&
+          timerItem.minutes == 0 &&
+          timerItem.seconds == 0) {
+        timer.cancel();
+        timerItem.isActive = false;
+      } else {
+        setState(() {
+          if (timerItem.seconds == 0) {
+            if (timerItem.minutes == 0) {
+              if (timerItem.hours > 0) {
+                timerItem.hours--;
+                timerItem.minutes = 59;
+                timerItem.seconds = 59;
               }
             } else {
-              _minutes1--;
-              _seconds1 = 59;
+              timerItem.minutes--;
+              timerItem.seconds = 59;
             }
           } else {
-            _seconds1--;
+            timerItem.seconds--;
           }
-        }
-      });
+        });
+      }
     });
-    _isActive1 = true;
-
-    _timer2 = Timer.periodic(oneSecond, (timer) {
-      setState(() {
-        if (_hours2 == 0 && _minutes2 == 0 && _seconds2 == 0) {
-          timer.cancel();
-          _isActive2 = false;
-        } else {
-          if (_seconds2 == 0) {
-            if (_minutes2 == 0) {
-              if (_hours2 > 0) {
-                _hours2--;
-                _minutes2 = 59;
-                _seconds2 = 59;
-              }
-            } else {
-              _minutes2--;
-              _seconds2 = 59;
-            }
-          } else {
-            _seconds2--;
-          }
-        }
-      });
-    });
-    _isActive2 = true;
+    timerItem.isActive = true;
   }
 
-  void _stopTimer() {
-    _timer1.cancel();
-    _timer2.cancel();
-    _isActive1 = false;
-    _isActive2 = false;
-  }
-
-  void _continueTimer() {
-    if (!_isActive1 || !_isActive2) {
-      _startTimer();
+  void _stopTimer(TimerItem timerItem) {
+    if (timerItem.timer?.isActive ?? false) {
+      timerItem.timer?.cancel();
+      timerItem.isActive = false;
     }
   }
 
-  void _resetTimer1() {
-    _timer1.cancel();
-    _hours1 = 0;
-    _minutes1 = 0;
-    _seconds1 = 0;
-    _hourController1.clear();
-    _minuteController1.clear();
-    _secondController1.clear();
+  void _resetTimer(TimerItem timerItem) {
+    setState(() {
+      timerItem.resetTimer();
+    });
   }
 
-  void _resetTimer2() {
-    _timer2.cancel();
-    _hours2 = 0;
-    _minutes2 = 0;
-    _seconds2 = 0;
-    _hourController2.clear();
-    _minuteController2.clear();
-    _secondController2.clear();
+  void _changeFontSize(TimerItem timerItem) {
+    if (timerItem.isActive == true) {
+      _timerfontsize = 60.0;
+    }
   }
+
+  // ... other methods such as _continueTimer will be updated similarly
 }
