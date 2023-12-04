582cd09e6f513da9118732221c0d2701c31a5093
Ahwu <ahwuiot@gmail.com>
2023-12-04 19:39:45 +0800

    123

--- Practice/web_crawler/lib/main.dart
+++ Practice/web_crawler/lib/main.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'package:html/parser.dart';
 
-void main() {
+void main() async {
+  var url = Uri.parse('https://tw.stock.yahoo.com/quote/2330');
+  var response = await http.get(url);
+  if (response.statusCode == 200) {
+    print('请求成功');
+    var document = parse(response.body);
+    var title = document.querySelector('h1')?.text;
+    print('找到标题:$title');
+  } else {
+    print('请求失败，状态码：${response.statusCode}');
+  }
   runApp(const MainApp());
 }
 
